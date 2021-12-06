4baac8cd740e2d8eb978430a80e2c306d5fc534e
Logan Boyd <github@loganboyd.com>
2021-12-06 14:14:04 -0600

    Update AOC_2021_Day_06.py

--- AOC_2021_Day_06.py
+++ AOC_2021_Day_06.py
@@ -1,30 +1,29 @@
-from collections import defaultdict
-from typing import DefaultDict
+from collections import deque
+from typing import Deque
 
 with open('2021/06_input.txt') as f:
     data = [int(n) for n in f.read().split(',')]
 
-def setup_fish() -> DefaultDict:
-    fish = defaultdict(int)
+def setup_fish(days: int) -> Deque:
+    fish = deque(maxlen=days)
+    for i in range(days):
+        fish.append(0)
     for d in data:
         fish[d] += 1
     return fish
 
-def process_day(f: DefaultDict):
+def process_day(f: Deque):
     new_fish = f[0]
-    f[0] = 0
-    for i in range(8):
-        f[i] += f[i+1]
-        f[i+1] = 0
+    f.rotate(-1)
     f[6] += new_fish
-    f[8] += new_fish
 
-def count_fish(f: DefaultDict, days: int) -> int:
+
+def count_fish(f: Deque, days: int) -> int:
     for day in range(days):
         process_day(f)
-    return sum(f.values())
+    return sum(f)
 
-fish = setup_fish()
+fish = setup_fish(9)
 print(f'Part 1: {count_fish(fish, 80)}')
-fish = setup_fish()
+fish = setup_fish(9)
 print(f'Part 2: {count_fish(fish, 256)}')
